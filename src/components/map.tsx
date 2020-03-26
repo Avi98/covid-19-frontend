a5dd6af548aaa9b3f26f17dbd5145d76239cf361
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-03-26 21:57:09 +0530

    refactored code

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -14,17 +14,53 @@ interface IResponseData {
 }
 
 const useFetchCovid = () => {
-    const [data, setData] = useState<covidArray>([]);
     useEffect(() => {
         const getData = async () => {
-            fetch('https://covid19.mathdro.id/api/deaths').then((res) => res.json()).then(res => setData(res))
+            const covidData:covidArray = await fetch('https://covid19.mathdro.id/api/deaths').then((res) => res.json()).then(res => res)
+            const svg = select('#map-container')
+            const projection = geoMercator().translate([750, 450]).scale(200);
+            const pathGenerator: any = geoPath().projection(projection);
 
-        }
-        getData()
 
-    }, [])
-    return [...data]
 
+            svg.append('path')
+                .attr('class', 'sphere')
+                .attr('d', pathGenerator({ type: 'Sphere' }))
+                
+
+            if (covidData.length > 0) {
+                debugger
+                const minVal = covidData && min(covidData, (d, i) => {
+                    return d.confirmed
+                })
+                const maxVal = covidData && max(covidData, (d, i) => {
+                    return d.confirmed
+                })
+                const colorScale = scaleLinear()
+                    //@ts-ignore
+                    .domain([minVal, maxVal])
+                    .range(["#faeaee", "#f08080", "#f57b7b",]);
+
+
+                json<IResponseData>('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+                    .then((data: any) => {
+                        const countries: any = feature(data, data.objects.countries);
+                        // merge covidData and countries.features 
+                        const mergedData = mergeData(covidData, countries.features)
+                        svg.selectAll('path').data(mergedData)
+                        .enter().append('path')
+                        .attr('class', 'country')
+                            .attr('d', pathGenerator)
+                            // .style('fill', () => '#f7f6f6')
+                            .style('fill', (d: any, i: any) => {
+                                const confirmed = d.confirmed;
+                                return confirmed ? colorScale(confirmed) : '#eeeef'
+                            });
+                    })
+            }}
+            getData()
+
+        }, [])
 }
 const SVG = styled.svg`
     height: 100vh;
@@ -45,50 +81,6 @@ const mergeData = (d1: any, d2: any) => {
 }
 export const Map: React.FC<IMap> = (props) => {
     const covidData = useFetchCovid()
-    useEffect(() => {
-        const svg = select('#map-container')
-        const projection = geoMercator().translate([750, 450]).scale(200);
-        const pathGenerator: any = geoPath().projection(projection);
-
-        
-
-        svg.append('path')
-            .attr('class', 'sphere')
-            .attr('d', pathGenerator({ type: 'Sphere' }))
-
-        if (covidData.length>0) {
-            debugger
-            const minVal =  covidData && min(covidData,(d,i)=>{
-                return d.confirmed
-            })
-            const maxVal = covidData && max(covidData, (d, i)=>{
-                return d.confirmed
-            })
-            const colorScale = scaleLinear()
-            //@ts-ignore
-            .domain([minVal, maxVal])
-            .range([ "#f1e4e7","#f08080", "#f57b7b", ]);
-
-
-            json<IResponseData>('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
-            .then((data: any) => {
-                const countries: any = feature(data, data.objects.countries);
-                // merge covidData and countries.features 
-                const mergedData = mergeData(covidData, countries.features)
-                svg.selectAll('path').data(mergedData)
-                    .enter().append('path')
-                    .attr('class', 'country')
-                    .attr('d', pathGenerator)
-                    .style('fill', ()=> '#ccc')
-                    .style('fill', (d: any, i: any) => {
-                        const confirmed = d.confirmed;
-                        return confirmed ? colorScale(confirmed): '#ccc'
-                    });
-            })
-        }
-    }, [covidData])
-
-
     return (
         <SVG id='map-container' />
     )
